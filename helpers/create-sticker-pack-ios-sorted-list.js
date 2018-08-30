37fdd900674cda247badabea503f44fba4db8944
Benedikt Groß <bg@benedikt-gross.de>
2018-08-30 13:54:24 +0200

    Fixed iOS sticker sorting to reflect better the offical Unicode recommendation

--- helpers/create-sticker-pack-ios-sorted-list.js
+++ helpers/create-sticker-pack-ios-sorted-list.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const _ = require('lodash');
 
+const emojisList = require('../data/openmoji.json');
+
 const folders = [
   "src/smileys-people/**/*.ai",
   "src/food-drink/**/*.ai",
@@ -15,12 +17,25 @@ const folders = [
   "src/symbols/**/*.ai",
 ];
 
+const arrayToEmojiDict = (array) => {
+  return array.reduce((o, a) => Object.assign(o, { [a.hexcode]: a }), {});
+}
+
+const emojiDict = arrayToEmojiDict(emojisList)
+
 let files = [];
 _.each(folders, f => {
   files = files.concat(glob(f));
 });
 files = _.map(files, f => {
-  return { filename: path.basename(f, '.ai') + '.sticker'};
+  const basename = path.basename(f, '.ai');
+  const emojiData = emojiDict[basename];
+  return { basename: basename, order: emojiData ? emojiData.order : 100000 }; // 100000 = some high number to put them last
+});
+// sort by offical unicode spec
+files = _.sortBy(files, ['order']);
+files = _.map(files, f => {
+  return { filename: f.basename + '.sticker'};
 });
 
 const sortedStickers = {
