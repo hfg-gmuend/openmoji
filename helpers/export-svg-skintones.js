5ea77c8b62897af33d7e0b6e753914e61572c592
Benedikt Groß <bg@benedikt-gross.de>
2019-05-07 16:20:10 +0200

    Added basic support for skintones multiple combinations

--- helpers/export-svg-skintones.js
+++ helpers/export-svg-skintones.js
@@ -13,7 +13,7 @@ const writeSvg = (filePath, data) => {
   fs.writeFileSync(filePath, data);
 }
 
-const generateSkintoneSvg = (srcFilePath, destFilePath, skintoneIndex) => {
+const generateSkintoneSingle = (srcFilePath, destFilePath, skintoneIndex) => {
   const dom = new JSDOM(fs.readFileSync(srcFilePath, 'utf8'));
   const doc = dom.window.document;
   const hairFills = doc.querySelectorAll('#hair [fill]');
@@ -36,6 +36,30 @@ const generateSkintoneSvg = (srcFilePath, destFilePath, skintoneIndex) => {
   writeSvg(destFilePath, doc.querySelector('svg').outerHTML);
 }
 
+const generateSkintoneMultiple = (srcFilePath, destFilePath, skintones) => {
+  const skintoneIndexA = skintones.split(',')[0] - 1;
+  const skintoneIndexB = skintones.split(',')[1] - 1;
+  const dom = new JSDOM(fs.readFileSync(srcFilePath, 'utf8'));
+  const doc = dom.window.document;
+
+  // TODO: currently works just for two skintone modifiers
+  doc.querySelectorAll('#skin-a [fill]').forEach(s => {
+    s.setAttribute('fill', fitzpatrickColors[skintoneIndexA]);
+  });
+  doc.querySelectorAll('#skin-a [stroke]').forEach(s => {
+    s.setAttribute('stroke', fitzpatrickColors[skintoneIndexA]);
+  });
+  doc.querySelectorAll('#skin-b [fill]').forEach(s => {
+    s.setAttribute('fill', fitzpatrickColors[skintoneIndexB]);
+  });
+  doc.querySelectorAll('#skin-b [stroke]').forEach(s => {
+    s.setAttribute('stroke', fitzpatrickColors[skintoneIndexB]);
+  });
+
+  doc.querySelector('#grid').remove();
+  writeSvg(destFilePath, doc.querySelector('svg').outerHTML);
+}
+
 const srcEmojis = require('../data/openmoji.json');
 let emojis = require('../data/openmoji.json');
 emojis = _.filter(emojis, (e) => { return e.skintone !== '' });
@@ -43,12 +67,21 @@ console.log('Export SVG Skintones: ' + emojis.length);
 
 emojis.forEach(e => {
   const skintoneBaseEmoji = _.find(srcEmojis, {'hexcode': e.skintone_base_hexcode});
-  // console.log(e.hexcode, e.skintone_base_hexcode);
-  generateSkintoneSvg(
-    path.join(folderSrc, skintoneBaseEmoji.group, skintoneBaseEmoji.subgroups, skintoneBaseEmoji.hexcode + '.svg'),
-    path.join(folderOut, e.hexcode + '.svg'),
-    e.skintone - 1 // fitzpatrick starts with 1 and not like an array with 0
-  );
+  // multiple skintone modifiers
+  if (e.skintone_combination === 'multiple') {
+    generateSkintoneMultiple(
+      path.join(folderSrc, skintoneBaseEmoji.group, skintoneBaseEmoji.subgroups, skintoneBaseEmoji.hexcode + '.svg'),
+      path.join(folderOut, e.hexcode + '.svg'),
+      e.skintone
+    );
+  // single skintone modifier
+  } else {
+    generateSkintoneSingle(
+      path.join(folderSrc, skintoneBaseEmoji.group, skintoneBaseEmoji.subgroups, skintoneBaseEmoji.hexcode + '.svg'),
+      path.join(folderOut, e.hexcode + '.svg'),
+      e.skintone - 1 // fitzpatrick starts with 1 and not like an array with 0
+    );
+  }
 });
 
 console.log('✅', emojis.length);
