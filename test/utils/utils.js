7dd5c339242f2676844f9670b1230e75e8160207
Benedikt Groß <bg@benedikt-gross.de>
2019-07-28 10:17:14 +0200

    Added parameter --openmoji-src-folder to run tests with a configurable path to src folder

--- test/utils/utils.js
+++ test/utils/utils.js
@@ -3,15 +3,18 @@ const path   = require('path');
 const _   = require('lodash');
 const JSDOM = require('jsdom').JSDOM;
 
+const argv = require('optimist').demand('openmoji-src-folder').argv;
+const openmojiSrcFolder = argv['openmoji-src-folder'];
+
 
 function createDoc(emoji) {
-  const svgFile = path.join('./src', emoji.group, emoji.subgroups, emoji.hexcode + '.svg');
+  const svgFile = path.join(openmojiSrcFolder, emoji.group, emoji.subgroups, emoji.hexcode + '.svg');
   const dom = new JSDOM(fs.readFileSync(svgFile), 'utf8');
   return dom.window.document;
 }
 
 function getSrcFilepath(emoji) {
-  return path.join('./src', emoji.group, emoji.subgroups, emoji.hexcode + '.svg');
+  return path.join(openmojiSrcFolder, emoji.group, emoji.subgroups, emoji.hexcode + '.svg');
 }
 
 module.exports.getSrcFilepath = getSrcFilepath;
