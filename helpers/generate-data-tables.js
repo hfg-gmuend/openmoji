815750e62378da3a78c2fb18b725b72b4f1ad369
Benedikt Groß <bg@benedikt-gross.de>
2019-12-28 11:35:00 -0500

    Updated emojibase-data 4.2.0 dependency (Emoji Unicode 12.1)

--- helpers/generate-data-tables.js
+++ helpers/generate-data-tables.js
@@ -96,7 +96,9 @@ emojis = _.orderBy(emojis, ['order', 'group', 'subgroups', 'hexcode'], ['asc', '
 
 // select all emojis which have not been designed yet (without skintones)
 const missingEmojis = _.filter(emojis, (e) => { return e.openmoji_author === '' && e.skintone === '' });
-// writeCsv(missingEmojis, 'data/openmoji-emoji12-missing.csv');
+if (missingEmojis.length > 0) {
+  writeCsv(missingEmojis, 'data/openmoji-emoji-unicode-missing.csv');
+}
 
 // remove all emojis which have not been designed yet
 emojis = _.filter(emojis, (e) => { return e.openmoji_author !== '' });
