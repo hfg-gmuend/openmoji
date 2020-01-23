f25daace6960f7342c50bff0d905e729ee2eca97
Benedikt Groß <bg@benedikt-gross.de>
2020-01-23 20:33:21 +0100

    Added tests to: ensure src svg is listed / hexcode is unique in openmoji.json #119

--- test/file-exists-src.js
+++ test/file-exists-src.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-const { filter } = require('lodash');
+const path = require('path');
+const { filter, find } = require('lodash');
 const { expect } = require('chai');
+const glob = require('glob').sync;
 
 const argv = require('optimist').demand('openmoji-data-json').argv;
 const openmojiDataJson = argv['openmoji-data-json'];
@@ -11,12 +13,37 @@ const { getSrcFilepath } = require('./utils/utils');
 
 describe('File integrity src files', function() {
   const emojis = filter(openmojis, (e) => { return e.skintone == ''});
+  const srcFiles = glob('src/**/*.svg');
 
-  describe('Source SVG exists?', function() {
+  describe('Source SVG files exist?', function() {
     emojis.forEach(emoji => {
       it(`${emoji.emoji} should have a source ${getSrcFilepath(emoji)}`, function(){
         const svgFile = getSrcFilepath(emoji);
         expect( fs.existsSync(svgFile) ).to.be.true;
+      });
+    });
+  });
+
+  describe('Source SVG files listed in openmoji.json?', function() {
+    srcFiles.forEach(f => {
+      const [srcFolder, group, subgroups, filename] = f.split(path.sep);
+      const hexcode = path.basename(filename, '.svg');
+      const openmoji = find(emojis, { 'hexcode': hexcode });
+      it(`${filename} should be listed in openmoji.json`, function(){
+        expect( openmoji ).to.exist;
+        expect( openmoji.group ).to.equal(group);
+        expect( openmoji.subgroups ).to.equal(subgroups);
+      });
+    });
+  });
+
+  describe('Every hexcode is unique in openmoji.json?', function() {
+    srcFiles.forEach(f => {
+      const [srcFolder, group, subgroups, filename] = f.split(path.sep);
+      const hexcode = path.basename(filename, '.svg');
+      const filtered = filter(emojis, { 'hexcode': hexcode });
+      it(`${hexcode} should be unique in openmoji.json`, function(){
+        expect( filtered.length ).to.equal(1);
       });
     });
   });
