3811fc0a9518db8f53453a8e7f341b7bd29dd152
Benedikt Groß <bg@benedikt-gross.de>
2020-04-29 16:08:19 +0200

    Fix and refactoring generate-fonts.sh

--- helpers/generate-fonts.sh
+++ helpers/generate-fonts.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -ueo pipefail
 
-# This script may be executed or sourced from any directory.
-cd -- "$(dirname -- "${BASH_SOURCE[0]}")"/../font || exit 1
+# -- prepare assets --
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"/.. || exit 1
 
 # copy and prepare svg assets for OpenMoji font generator
-echo "👉 export-svg-font.js"
-helpers/export-svg-font.js
+echo "👉 generate-font-glyphs.js"
+helpers/generate-font-glyphs.js
 
 # generate css file for OpenMoji fonts
 echo "👉 generate-font-css.js"
 helpers/generate-font-css.js
 
-# OpenMoji font generator via scfbuild Docker
+# -- OpenMoji font generator via scfbuild Docker --
+cd -- "$(dirname -- "${BASH_SOURCE[0]}")"/../font || exit 1
+
 IMAGE='scfbuild:latest'
 NAME='scfbuild_bash'
 
