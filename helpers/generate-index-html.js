28511ff0fd6f5f6edfe35538e6b46c9e49311b32
Carlin MacKenzie <carlin.mackenzie@gmail.com>
2020-05-19 20:35:57 -0400

    redesign toggles and add SVGs
    issues:
       - it is now really slow as it is 30k lines long
       - it's non-trivial to show the black version of system font
         I tried attaching VS15 to emoji but parseInt discards it
         Alternative option is passing a list of fonts like "Segoe UI
         Symbol" but I would need to find the list

--- helpers/generate-index-html.js
+++ helpers/generate-index-html.js
@@ -71,28 +71,61 @@ button p {
 	font-size: 44px;
 	margin: 0;
 }
-.toggle {
+#toggles {
+    display: grid;
+    grid-template-columns: auto auto;
     position: fixed;
+    bottom: 0;
+    left: 0;
+    width: auto;
+    padding: 2.5px;
+}
+.toggle {
+    position: static;
     background-color: #aaa;
     color: var(--background-color-body);
     padding: 5px;
+    margin: 2.5px;
     border-radius: 5px;
     height: 25px
 }
-#systemToggle {
+.selected {
+    background: #666;
+}
+button.toggle  {
+    width: auto;
+    height: 35px;
+    color: var(--background-color-body);
+}
+button.toggle:hover {
+    background: #bbb;
+    transform: none;
+}
+button.toggle:focus {
+	outline: 1px solid #fff;
+	outline-offset: -4px;
+	background: #666;
+	transform: none;
+}
+button.toggle:active {
+    transform: scale(0.95);
+    background: #666;
+}
+#pngsvgToggle {
     bottom: 100px;
     left: 10px;
 }
-#fontToggle {
+#openmojisystemToggle {
     bottom: 55px;
     left: 10px;
 }
-#blackToggle {
+#colorblackToggle {
     bottom: 10px;
     left: 10px;
 }
 #backgroundToggle {
-    bottom: 10px; 
+    position: fixed;
+    bottom: 10px;
     right: 10px;
 }
 .omBlack p {
@@ -116,10 +149,10 @@ button p {
     #toggles {
         display: flex;
         flex-direction: row;
-        position: fixed;
-        bottom: 0;
         width: 100%;
-        padding: 2.5px;
+    }
+    #backgroundToggle {
+        position: static;
     }
     #color, #black, #system {
         margin: 0 1vw;
@@ -139,7 +172,7 @@ button p {
 <p style='text-align: center; padding: 15px; font-style: italic; color: #999;'> click to copy codepoint </p>
 `;
 
-html += `<div id='color'>`
+html += `<div id='pngColor'>`
 html += _.map(emojisList, (e) => {
     if (e.skintone === '') {
         return `<button onclick="copyToClipboard('${e.hexcode}')">
@@ -149,13 +182,35 @@ html += _.map(emojisList, (e) => {
     }
 }).join('');
 
-html += `</div><div id='black' style='display:none;'>`
+html += `</div><div id='pngBlack' style='display:none;'>`
 
 html += _.map(emojisList, (e) => {
     if (e.skintone === '') {
         return `<button onclick="copyToClipboard('${e.hexcode}')">
         <img alt="${e.annotation}" title="${e.annotation} - ${e.hexcode}"
         src="${'black/72x72/' + e.hexcode +'.png'}" height="72" width="72">
+    </button>`;
+    }
+}).join('');
+
+html += `</div><div id='svgColor' style='display:none;'>`
+
+html += _.map(emojisList, (e) => {
+    if (e.skintone === '') {
+        return `<button onclick="copyToClipboard('${e.hexcode}')">
+        <img alt="${e.annotation}" title="${e.annotation} - ${e.hexcode}"
+        src="${'color/svg/' + e.hexcode +'.svg'}" height="72" width="72">
+    </button>`;
+    }
+}).join('');
+
+html += `</div><div id='svgBlack' style='display:none;'>`
+
+html += _.map(emojisList, (e) => {
+    if (e.skintone === '') {
+        return `<button onclick="copyToClipboard('${e.hexcode}')">
+        <img alt="${e.annotation}" title="${e.annotation} - ${e.hexcode}"
+        src="${'black/svg/' + e.hexcode +'.svg'}" height="72" width="72">
     </button>`;
     }
 }).join('');
@@ -172,20 +227,14 @@ html += _.map(emojisList, (e) => {
 
 html += `</div>
 <div id='toggles'>
-    <div class='toggle' id='systemToggle'>
-        <input type="checkbox" id="openmojisystemCheckbox"/>
-        <label for="openmojisystemCheckbox" id="openmojisystemToggle"> Toggle System Emojis</label>
-    </div>
+    <button class='toggle selected' id='pngT'>PNG</button>
+    <button class='toggle' id='svgT'>SVG</button>
 
-    <div class='toggle' id='fontToggle'>
-        <input type="checkbox" id="openmojifontCheckbox"/>
-        <label for="openmojifontCheckbox" id="openmojifontToggle"> Toggle OpenMoji Font</label>
-    </div>
+    <button class='toggle' id='openmojiT'>OpenMoji</button>
+    <button class='toggle' id='systemT'>System</button>
 
-    <div class='toggle' id='blackToggle'>
-        <input type="checkbox" id="colorblackCheckbox"/>
-        <label for="colorblackCheckbox" id="colorblackToggle"> Toggle Black Emojis</label>
-    </div>
+    <button class='toggle selected' id='colorT'>Color</button>
+    <button class='toggle' id='blackT'>Black</button>
 
     <div class='toggle' id='backgroundToggle'>
         <input type="checkbox" id="modeCheckbox"/>
@@ -194,61 +243,87 @@ html += `</div>
 </div>
 
 <script>
-const colorblackToggle = document.getElementById('colorblackCheckbox');
-
-colorblackToggle.addEventListener('change', () => {
-    if (colorblackToggle.checked) {
-        document.getElementById('black').style = "";
-        document.getElementById('color').style = "display:none";
-        document.getElementById('system').style = "display:none";
+function toggle(t) {
+    if (t == 'blackT' || t == 'colorT') {
+        document.getElementById('colorT').classList.remove('selected');
+        document.getElementById('blackT').classList.remove('selected');
     } else {
-        document.getElementById('black').style = "display:none";
-        if (openmojisystemToggle.checked) {
+        document.getElementById('pngT').classList.remove('selected');
+        document.getElementById('svgT').classList.remove('selected');
+        document.getElementById('openmojiT').classList.remove('selected');
+        document.getElementById('systemT').classList.remove('selected');
+    }
+    document.getElementById(t).classList.add('selected');
+
+    const state = [...
+        document.querySelectorAll('.selected')
+    ].map(i => i.innerHTML);
+
+    switch (state[0]) {
+        case 'PNG':
+            document.getElementById('system').style = "display:none";
+            document.getElementById('svgBlack').style = "display:none";
+            document.getElementById('svgColor').style = "display:none";
+            if (state[1] == 'Black') {
+                // black PNG
+                document.getElementById('pngBlack').style = "";
+                document.getElementById('pngColor').style = "display:none";
+            } else {
+                // color PNG
+                document.getElementById('pngBlack').style = "display:none";
+                document.getElementById('pngColor').style = "";
+            }
+            break;
+        case 'SVG':
+            document.getElementById('system').style = "display:none";
+            document.getElementById('pngBlack').style = "display:none";
+            document.getElementById('pngColor').style = "display:none";
+            if (state[1] == 'Black') {
+                // black SVG
+                document.getElementById('svgBlack').style = "";
+                document.getElementById('svgColor').style = "display:none";
+            } else {
+                // color SVG
+                document.getElementById('svgBlack').style = "display:none";
+                document.getElementById('svgColor').style = "";
+            }
+            break;
+        case 'OpenMoji':
+            document.getElementById('pngBlack').style = "display:none";
+            document.getElementById('pngColor').style = "display:none";
+            document.getElementById('svgBlack').style = "display:none";
+            document.getElementById('svgColor').style = "display:none";
+
             document.getElementById('system').style = "";
-        } else {
-            document.getElementById('color').style = "";
-        }
-    }
-});
+            if (state[1] == 'Black') {
+                // black OpenMoji
+                document.getElementById('system').classList.add("omBlack");
+                document.getElementById('system').classList.remove("omColor");
+            } else {
+                // color OpenMoji
+                document.getElementById('system').classList.remove("omBlack");
+                document.getElementById('system').classList.add("omColor");
+            }
+            break;
+        case 'System':
+            document.getElementById('pngBlack').style = "display:none";
+            document.getElementById('pngColor').style = "display:none";
+            document.getElementById('svgBlack').style = "display:none";
+            document.getElementById('svgColor').style = "display:none";
 
-const openmojifontToggle = document.getElementById('openmojifontCheckbox');
-
-openmojifontToggle.addEventListener('change', () => {
-    if (openmojifontToggle.checked) {
-        document.getElementById('system').style = "";        
-        if (colorblackToggle.checked) {
-            document.getElementById('system').classList.add("omBlack");
-        } else {
-            document.getElementById('system').classList.add("omColor");
-        }
-        document.getElementById('color').style = "display:none";
-        document.getElementById('black').style = "display:none";
-    } else {
-        document.getElementById('system').classList.remove("omBlack");
-        document.getElementById('system').classList.remove("omColor");
-        if (openmojisystemToggle.checked) {
             document.getElementById('system').style = "";
-        } else {
-            document.getElementById('color').style = "";
-        }
+            document.getElementById('system').classList.remove("omBlack");
+            document.getElementById('system').classList.remove("omColor");
+            if (state[1] == 'Black') {
+                console.log('black System')
+            } else {
+                console.log('color System')
+            }
+            break;
+        default:
+            console.log('error:', state)
     }
-});
-const openmojisystemToggle = document.getElementById('openmojisystemCheckbox');
-
-openmojisystemToggle.addEventListener('change', () => {
-    if (openmojisystemToggle.checked) {
-        document.getElementById('system').style = "";
-        document.getElementById('color').style = "display:none";
-        document.getElementById('black').style = "display:none";
-    } else {
-        document.getElementById('system').style = "display:none";
-        if (colorblackToggle.checked) {
-            document.getElementById('black').style = "";
-        } else {
-            document.getElementById('color').style = "";
-        }
-    }
-});
+}
 
 const modeToggle = document.getElementById('modeCheckbox');
 
@@ -268,6 +343,25 @@ function copyToClipboard(value) {
     document.execCommand("copy");
     temp.parentNode.removeChild(temp);
 }
+
+document.getElementById('pngT').onclick = () => {
+    toggle('pngT')
+};
+document.getElementById('svgT').onclick = () => {
+    toggle('svgT')
+};
+document.getElementById('openmojiT').onclick = () => {
+    toggle('openmojiT')
+};
+document.getElementById('systemT').onclick = () => {
+    toggle('systemT')
+};
+document.getElementById('colorT').onclick = () => {
+    toggle('colorT')
+};
+document.getElementById('blackT').onclick = () => {
+    toggle('blackT')
+};
 </script>
 </body>
 </html>
