2f42f81bf803e8d9101203dbb057f3e6c7af16e0
Peter Oliver <git@mavit.org.uk>
2020-08-21 15:44:05 +0100

    Build COLR TTFs with nanoemoji
    This is done via the container at https://gitlab.com/mavit/nanoemoji-container.
    
    For me, the resulting COLR TTFs work in Firefox and in the Gnome desktop
    on Fedora 32 (although they display rather small).
    
    Caveats:
    - It may be premature to replace `scfbuild` for SVG builds.  If we decide to go ahead without `scfbuild`, then we’ll want to remove the remaining files related to it.  If we decide to keep `scfbuild`, then we’ll want to revert those parts of this patch.  In either case we’ll want to update the readme to link to the new outputs.
    - I’ve tested this with Podman, not Docker.
    
    Relates to #93.

--- helpers/generate-fonts.sh
+++ helpers/generate-fonts.sh
@@ -4,28 +4,62 @@ set -ueo pipefail
 # -- prepare assets --
 cd -- "$(dirname -- "${BASH_SOURCE[0]}")"/.. || exit 1
 
-# copy and prepare svg assets for OpenMoji font generator
-echo "👉 generate-font-glyphs.js"
-helpers/generate-font-glyphs.js
-
 # generate css file for OpenMoji fonts
 echo "👉 generate-font-css.js"
 helpers/generate-font-css.js
 
-# -- OpenMoji font generator via scfbuild Docker --
-cd -- "$(dirname -- "${BASH_SOURCE[0]}")"/../font || exit 1
+sed 's/Color/Black/;' font/OpenMoji-Color.ttx > font/OpenMoji-Black.ttx
 
-IMAGE='scfbuild:latest'
-NAME='scfbuild_bash'
 
-# is docker container "scfbuild_bash" running?
-# if not ... start container
-[[ "$(docker ps -f "name=$NAME" --format '{{.Names}}')" == "$NAME" ]] ||
-docker run --name "$NAME" --rm -t -d --volume "$PWD":/wd --workdir /wd "$IMAGE" bash
+# -- OpenMoji COLR TTF font generator via nanoemoji container --
+version=$(git describe --tags)
 
-# generate fonts
-docker exec -ti "$NAME" bash -c "/scfbuild/bin/scfbuild -c /wd/scfbuild-color.yml"
-docker exec -ti "$NAME" bash -c "/scfbuild/bin/scfbuild -c /wd/scfbuild-black.yml"
+# If we're connected to a terminal, don't flood it with ninja output,
+# and enable ^C.
+if [[ -t 1 ]]; then
+    tty='--tty --interactive'
+else
+    tty=''
+fi
 
-# stop container
-docker stop "$NAME"
+# FIXME: Upgrade glyf_colr_0 to glyf_colr_1 once
+# https://github.com/googlefonts/colr-gradients-spec stabilises.
+#
+# FIXME: Switch untouchedsvgz for picosvgz when
+# https://github.com/googlefonts/nanoemoji/issues/113 is fixed.
+#
+# FIXME: Here we build OpenMoji Black as a colour font that just happens
+# to use black as the only colour.  This is probably a bad idea.
+# https://github.com/googlefonts/nanoemoji/issues/114
+#
+for saturation in black color; do
+    name=OpenMoji-${saturation^}
+    build_dir=/mnt/build/$saturation
+
+    for format in glyf_colr_0 untouchedsvgz; do
+        mkdir -p font/$format
+
+        docker run \
+            --mount=type=bind,source="$PWD",destination=/mnt,relabel=private \
+            --rm \
+            $tty \
+            registry.gitlab.com/mavit/nanoemoji-container:master \
+            sh -c "
+                set -o errexit
+
+                nanoemoji \
+                    --color_format=$format \
+                    --build_dir=$build_dir \
+                    --output_file=$build_dir/$name.$format.ttf \
+                    /mnt/$saturation/svg/*.svg
+                xmlstarlet edit --inplace --update \
+                    '/ttFont/name/namerecord[@nameID=\"5\"][@platformID=\"3\"]' \
+                    --value '$version' \
+                    /mnt/font/$name.ttx
+                ttx \
+                    -m $build_dir/$name.$format.ttf \
+                    -o /mnt/font/$format/$name.ttf \
+                    /mnt/font/$name.ttx
+            "
+    done
+done
