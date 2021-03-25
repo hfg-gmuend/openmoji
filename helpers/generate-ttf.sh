0cc1bc694d51037294337e8b877af98ed0e1a050
Peter Oliver <git@mavit.org.uk>
2021-03-25 22:55:12 +0000

    Build with nanoemoji 0.7.1
    Make use of the new `--ascender` and `--descender` options to control the size of the characters.

--- helpers/generate-ttf.sh
+++ helpers/generate-ttf.sh
@@ -11,14 +11,6 @@ build_dir=$5
 name=OpenMoji-${saturation^}
 
 mkdir -p "$build_dir"
-rsync -ru "/mnt/$saturation/svg/" "$build_dir/scale/"
-grep -FL '<g transform="translate(36 0) scale(1.3) translate(-36 0)">' \
-     "$build_dir"/scale/*.svg \
-  | xargs --no-run-if-empty \
-          sed -E -i \
-              -e 's/(<svg .*>)/\1\n<g transform="translate(36 0) scale(1.3) translate(-36 0)">/;' \
-              -e 's/(<\/svg>)/<\/g>\n\1/;'
-
 cat >"$build_dir/$name.toml" <<-EOF
 	output_file = "$build_dir/$name.$method.ttf"
 	color_format = "$method"
@@ -29,14 +21,16 @@ cat >"$build_dir/$name.toml" <<-EOF
 
 	[master.regular]
 	style_name = "Regular"
-	srcs = ["$build_dir/scale/*.svg"]
+	srcs = ["/mnt/$saturation/svg/*.svg"]
 
 	[master.regular.position]
 	wght = 400
 EOF
 
 nanoemoji --build_dir="$build_dir" \
-          --config="$build_dir/$name.toml"
+          --config="$build_dir/$name.toml" \
+          --ascender=1045 \
+          --descender=-275
 
 sed "s/Color/${saturation^}/;" \
     /mnt/font/OpenMoji-Color.ttx \
