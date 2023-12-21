78b7ed6980d1ba1d68d79b6a325ab93662d1c76a
Benedikt Groß <bg@benedikt-gross.de>
2023-12-21 08:49:35 +0100

    Fix broken helpers/export-zip.sh as of new structure of /font folder

--- helpers/export-zip.sh
+++ helpers/export-zip.sh
@@ -16,16 +16,6 @@ helpers/lib/create-zip.sh black/618x618/ _tmp/openmoji-618x618-black.zip
 # -- fonts --
 (
   cd font
-  zip -x ".*" -x "__MACOSX" \
-      glyf/OpenMoji-Black.ttf \
-      glyf/demo.html \
-      glyf/openmoji.css \
-      glyf_colr_0/OpenMoji-Color.ttf \
-      glyf_colr_0/demo.html \
-      glyf_colr_0/openmoji.css \
-      scfbuild/OpenMoji-Color.ttf \
-      scfbuild/demo.html \
-      scfbuild/openmoji.css \
-      openmoji-font.zip
+  zip -r openmoji-font.zip . -x ".*" -x "__MACOSX"
   mv openmoji-font.zip ../_tmp/
 )
